f12af2655a2c26810cc8963a68e8bd69b58bdd72
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-17 01:59:02 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,22 @@
 'use strict';
+
+class Question {
+
+  constructor(question){
+    this.question = question
+    this.save()
+  }
+
+  save(){
+    this.constructor._All.push(this)
+  }
+
+  static All(){
+    return this._All
+  }
+   static Find()
+  return this.All().filter(function(question){
+     return Question.question[i] === question[i+1]
+     //
+}
+Question._All = []
